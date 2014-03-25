29d539cfd8c65f28f3570c050f41865189b64c89
ashiguruma <tomloudon@gmail.com>
2014-03-25 08:41:32 +0000

    Created blank project and added a presentation and assets (cats)

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+
+
+
+    });
+
+    /**
+     * Grunt Task Modules
+     *
+     * Grunt modules used by the
+     * tasks
+     *
+     * npm install {module_name}
+     */
+    grunt.loadNpmTasks('');
+
+
+    /**
+     * Grunt Task Groups
+     *
+     * Task groups to run multiple
+     * tasks in one go
+     */
+    // grunt.registerTask('', []);
+
+};
---
+++ README.md
@@ -0,0 +1,9 @@
+# Simple Blank Project
+
+    npm init
+
+    npm install grunt
+
+    npm install -g bower
+
+    bower init
